62d454fc593c160ec3878f6251f9fa8dc689ed4a
myprj22 <sophie2ya@gmail.com>
2020-10-22 12:49:29 +0900

    처음 저장소에 올림

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const mongoose = require('mongoose')
+mongoose.connect('mongodb+srv://admin:admin1234@cluster0.dt0ai.mongodb.net/<dbname>?retryWrites=true&w=majority',{
+    useNewUrlParser: true, useUnifiedTopology: true,useCreateIndex: true, useFindAndModify: false
+}).then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err))
+
+
+
+app.get('/', (req, res) => {
+  res.send('안녕하세요~~')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
